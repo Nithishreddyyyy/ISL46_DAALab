35ce1845a461cb249dd39094e7b4055bcb281104
Nithish Reddy <nithishreddyb2@gmail.com>
2025-05-29 17:49:54 +0530

    second part

--- 2_TimeComplexity.py
+++ 2_TimeComplexity.py
@@ -36,3 +36,43 @@ if __name__ == "__main__":
   plt.show()
 
 
+#b. Find the binary equivalent of a given decimal number.
+
+import time as t
+import matplotlib.pyplot as plt
+
+def decimal_to_binary(n):
+    if n == 0:
+        return "0"
+
+    binary = ""
+    while n > 0:
+        remainder = n % 2
+        binary = str(remainder) + binary
+        n = n // 2
+    return binary
+
+'''
+if __name__ == "__main__":
+    x = []
+    y = []
+
+    for num in range(10, 1001, 50):
+        start = t.time()
+        binary_val = decimal_to_binary(num)
+        end = t.time()
+        gap = end - start
+
+        x.append(num)
+        y.append(gap)
+
+        print("Binary equivalent of " + str(num) + " is: " + binary_val)
+
+    plt.figure(figsize=(10,6))
+    plt.plot(x, y,color='orange', label="Decimal to Binary Conversion Time")
+    plt.xlabel("Decimal Input Value")
+    plt.ylabel("Time (seconds)")
+    plt.title("Time complexcity of decimal to binary conversion")
+    plt.legend()
+    plt.show()
+'''
