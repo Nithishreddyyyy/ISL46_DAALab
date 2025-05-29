280c61bad147a612d3c96e260609668a165ba2b7
Nithish Reddy <nithishreddyb2@gmail.com>
2025-05-29 18:22:43 +0530

    added code

---
+++ 4_Merge_Sort.py
@@ -0,0 +1,61 @@
+#Merge Sort
+import matplotlib.pyplot as plt
+import time as t
+import random
+
+def divide(a, low, high):
+    if low < high:
+        mid = (high + low) // 2
+        divide(a, low, mid)
+        divide(a, mid + 1, high)
+        merge(a, low, mid, high)
+
+def merge(a, low, mid, high):
+    temp = [0] * (high - low + 1)
+    i = low
+    j = mid + 1
+    k = 0
+
+    while i <= mid and j <= high:
+        if a[i] <= a[j]:
+            temp[k] = a[i]
+            i += 1
+        else:
+            temp[k] = a[j]
+            j += 1
+        k += 1
+
+    while i <= mid:
+        temp[k] = a[i]
+        i += 1
+        k += 1
+
+    while j <= high:
+        temp[k] = a[j]
+        j += 1
+        k += 1
+
+    for i in range(low, high + 1):
+        a[i] = temp[i - low]
+
+if __name__ == "__main__":
+    x = []
+    y = []
+
+    for n in range(10000, 100001, 10000):
+        a = [random.randint(1, n) for _ in range(n)]
+        x.append(n)
+
+        start = t.time()
+        divide(a, 0, len(a) - 1)
+        end = t.time()
+
+        y.append(end - start)
+
+    plt.plot(x, y, marker='o', color='purple', label="Merge Sort")
+    plt.xlabel("Input size (n)")
+    plt.ylabel("Time (seconds)")
+    plt.title("Time Complexity of Merge Sort")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
