63cecec2ea60997e8b35152c0d077abbeeab6357
Nithish Reddy <nithishreddyb2@gmail.com>
2025-07-02 00:16:19 +0530

    Updated File

--- 8_Knapsack.py
+++ 8_Knapsack.py
@@ -7,7 +7,8 @@ def knapsack(wt, val):
     for i in range(1, N + 1):
         for w in range(W + 1):
             if wt[i - 1] <= w:
-                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w])
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], 
+                               dp[i - 1][w])
             else:
                 dp[i][w] = dp[i - 1][w]
 
