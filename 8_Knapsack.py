639a4852ff5997968b79b6c7bec6554e161b2a4e
Nithish Reddy <nithishreddyb2@gmail.com>
2025-07-01 20:08:28 +0530

    updated code

--- 8_Knapsack.py
+++ 8_Knapsack.py
@@ -1,25 +1,20 @@
-def knapsack(W, wt, val, n):
-    dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+N = 4   # Number of items
+W = 7   # Capacity of the bag
 
-    for i in range(1, n + 1):
+def knapsack(wt, val):
+    dp = [[0] * (W + 1) for _ in range(N + 1)]
+
+    for i in range(1, N + 1):
         for w in range(W + 1):
-            if wt[i-1] <= w:
-                dp[i][w] = max(val[i-1] + dp[i-1][w - wt[i-1]], dp[i-1][w])
+            if wt[i - 1] <= w:
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w])
             else:
-                dp[i][w] = dp[i-1][w]
+                dp[i][w] = dp[i - 1][w]
 
-    print("Maximum Profit:", dp[n][W])
-    print("Selected item indices (0-based): ", end="")
-    w = W
-    for i in range(n, 0, -1):
-        if dp[i][w] != dp[i-1][w]:
-            print(i-1, end=" ")
-            w -= wt[i-1]
-    print()
+    print(f"Maximum profit = {dp[N][W]}")
 
-profit = [0,12,10,20,15]
-weight = [0,2,1,3,2]
-capacity = 50
-n = len(profit)
+# Inputs
+weights = [1, 3, 4, 5]
+values = [1, 4, 5, 7]
 
-knapsack(capacity, weight, profit, n)
+knapsack(weights, values)
